ddd7735d76f4e2007fd3f82cf01b5a09dd917c9b
tun71400 <77815590+tun71400@users.noreply.github.com>
2021-09-22 09:42:53 -0400

    Added error msg's for missing fields or passwords not matching.

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -2,10 +2,52 @@ package edu.temple.signupform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Debug
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import java.util.*
+
 
 class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var name = findViewById<EditText>(R.id.editTextTextPersonName2)
+        var email = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        var password = findViewById<EditText>(R.id.editTextTextPassword)
+        var passwordChecker = findViewById<EditText>(R.id.editTextTextPassword2)
+        var saveButton = findViewById<Button>(R.id.button)
+
+
+
+        saveButton.setOnClickListener {
+
+            if (name.text.toString() == ""){
+                name.setError("Name is a required field")
+            }
+            else if(email.text.toString() == ""){
+                email.setError("Email is a required field")
+            }
+            else if(password.text.toString() == ""){
+                password.setError("Password is a required field")
+            }
+            else if(passwordChecker.text.toString() == ""){
+                passwordChecker.setError("Confirm Password is a required field")
+            }
+            else if(password.text.toString() != passwordChecker.text.toString()){
+                passwordChecker.setError("Password and Confirm Password must match")
+            }
+            else {
+                var toast = Toast.makeText(this, "Welcome, " + name.text.toString() + ", to the SignUpForm App", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
+
+
     }
 }
